c9c136edfd92f1b5b4b082e3671611007c6faa2b
Janrelle24 <lubianojanrelle@gmail.com>
2023-10-10 16:03:22 +0800

    create component start vue and link to the app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    <button class="btn btn-primary">Primary Button</button>
+  <div v-if="status === 'beginning'" class="height-100 d-flex justify-content-center align-items-center">
+    <Start @start-game="startGame"/>
   </div>
 </template>
+
+<script>
+import Start from "./components/Start.vue";
+
+export default{
+  name: "App",
+  components: {
+    Start
+  },
+  data(){
+    return {
+      status: "beginning",
+      size: 3,
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+.height-100 {
+  height: 100vh;
+}
+</style>
 
---
+++ src/components/Start.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="text-center pt-5">
+        <div class="pt-5">
+            <input type="radio" name="level" id="easy" :value="3" v-model="level" checked/>
+            <label class="me-4" for="easy">
+                Easy
+            </label>
+        
+        
+            <input type="radio" name="level" id="medium" :value="4" v-model="level"/>
+            <label class="me-4" for="medium">
+                Medium
+            </label>
+        
+        
+            <input type="radio" name="level" id="hard" :value="5" v-model="level"/>
+            <label class="me-4" for="hard">
+                Hard
+            </label>
+        </div>
+        <div>
+            <button type="button" class="btn btn-success mt-4" @click="$emit('start-game', this.level)">START</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        name: "Start",
+        data() {
+            return{
+                level: 3,
+            };
+        },
+    };
+</script>
+
+<style>
+label {
+    font-size: 18px;
+}
+
+button {
+    padding: 10px 35px !important;
+    font-size: 20px !important;
+}
+
+input {
+    margin-right: 10px !important;
+}
+</style>
